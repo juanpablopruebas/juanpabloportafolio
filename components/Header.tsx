8e9af94f9d1ca9a013db21d0e2157f35124fa7ce
Juan Pablo <jpvjpruebas1@gmail.com>
2025-08-03 23:14:59 -0500

    Refactor Header component and update next config

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 export default function Header() {
+  const handleCopy = (text: string) => {
+    navigator.clipboard.writeText(text);
+  };
   return (
     <header className="py-10 px-4 text-center bg-zinc-700 shadow-md">
       <h1 className="text-5xl font-extrabold mb-2 text-emerald-400">
@@ -6,8 +11,20 @@ export default function Header() {
       </h1>
       <p className="text-xl mb-6 text-gray-300">Frontend Developer</p>
       <div className="text-base space-y-1">
-        <p className="text-gray-300">📧 juanpablovj2030@gmail.com</p>
-        <p className="text-gray-300">🔗 linkedin.com/in/juan-pablo-villalba-jaramillo</p>
+        <a
+          className="text-gray-300 hover:text-emerald-400 block"
+          href="mailto:juanpablovj2030@gmail.com"
+        >
+          📧 juanpablovj2030@gmail.com
+        </a>
+        <a
+          href="https://linkedin.com/in/juan-pablo-villalba-jaramillo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 hover:text-emerald-400 block"
+        >
+          🔗 linkedin.com/in/juan-pablo-villalba-jaramillo
+        </a>
       </div>
     </header>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/juanvillalbaportfolio",
-  assetPrefix: "/juanvillalbaportfolio",
 };
 
 export default nextConfig;
